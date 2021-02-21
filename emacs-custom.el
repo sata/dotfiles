65c6ce4730b55bed181f307cd81e0ff34ba02191
Sam Tavakoli <sam@tavakoli.se>
2021-02-21 18:09:51 +0000

    Adding smex to selected packages

--- emacs-custom.el
+++ emacs-custom.el
@@ -31,7 +31,7 @@
  '(org-log-done 'time)
  '(org-roam-completion-system 'ido)
  '(package-selected-packages
-   '(cargo cc-mode ccls cl-lib company company-go company-lsp cyberpunk-theme eglot elixir-mode exunit flycheck flycheck-golangci-lint flycheck-plantuml flycheck-rust go-mode go-projectile go-tag gotest helm-lsp highlight-symbol lsp-mode lsp-treemacs lsp-ui lua-mode magit markdown-mode nix-mode nyan-mode org-roam package+ plantuml-mode projectile python-mode ruby-mode rust-mode ssh terraform-mode toml-mode use-package vterm web-mode yaml-mode yasnippet))
+   '(smex cargo cc-mode ccls cl-lib company company-go company-lsp cyberpunk-theme eglot elixir-mode exunit flycheck flycheck-golangci-lint flycheck-plantuml flycheck-rust go-mode go-projectile go-tag gotest helm-lsp highlight-symbol lsp-mode lsp-treemacs lsp-ui lua-mode magit markdown-mode nix-mode nyan-mode org-roam package+ plantuml-mode projectile python-mode ruby-mode rust-mode ssh terraform-mode toml-mode use-package vterm web-mode yaml-mode yasnippet))
  '(projectile-mode t nil (projectile))
  '(ring-bell-function 'ignore)
  '(scroll-bar-mode nil)
