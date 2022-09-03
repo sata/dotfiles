03d17ed80a5335acc60c8a9558f716d352eaca4c
Sam Tavakoli <gitspam@tavakoli.se>
2022-09-03 14:43:24 +0100

    Increase limits for Emacs

--- early-init.el
+++ early-init.el
@@ -2,3 +2,5 @@
 (push '(vertical-scroll-bars) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
